6229b18f5a95af73249cdf4348470af77b92e97f
Neil S Primmer <neil@neilprimmer.com>
2023-10-30 10:08:21 -0400

    More printing

--- frontend/src/DocViewer.js
+++ frontend/src/DocViewer.js
@@ -8,7 +8,15 @@ const docContext = require.context('./docs', true, /\.md$/);
 function DocViewer() {
   const { docName } = useParams();
   const markdownContent = docContext(`./${docName}`).default;
+  console.log(docContext.keys());
     console.log(markdownContent);
+    let markdownContent2;
+    try {
+      markdownContent2 = require(`./docs/${docName}`).default;
+    } catch (err) {
+      console.error("Error importing the markdown file:", err);
+    }
+    
   return (
     <div>
         <h3>Document Name: {docName}</h3>
