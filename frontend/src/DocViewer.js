e1f93042b7023c6990b93296f9c91f32e2c4a972
Neil S Primmer <neil@neilprimmer.com>
2023-10-30 09:26:39 -0400

    Rendering printlining

--- frontend/src/DocViewer.js
+++ frontend/src/DocViewer.js
@@ -8,9 +8,10 @@ const docContext = require.context('./docs', true, /\.md$/);
 function DocViewer() {
   const { docName } = useParams();
   const markdownContent = docContext(`./${docName}`).default;
-
+    console.log(markdownContent);
   return (
     <div>
+        <h3>Document Name: {docName}</h3>
       <ReactMarkdown remarkPlugins={[gfm]} children={markdownContent} />
     </div>
   );
